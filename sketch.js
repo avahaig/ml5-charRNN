7b7d32da77d8b1a588ca17f42417fd80fbbaa063
avahaig <haiga@emmanuel.edu>
2022-03-28 19:17:05 +0000

    updates

--- sketch.js
+++ sketch.js
@@ -5,3 +5,15 @@ function setup () {
 function draw () {
   // put drawing code here
 }
+// Create the character level generator with a pre trained model
+const rnn = ml5.charRNN('models/bolaño/', modelLoaded);
+
+// When the model is loaded
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+
+// Generate content
+rnn.generate({ seed: 'the meaning of pizza is' }, (err, results) => {
+  console.log(results);
+});
